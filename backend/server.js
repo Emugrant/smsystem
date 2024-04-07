22f72c7997fa2e6617efbccd5ba0267302e29e5d
Emmanuel <gazellehunter24@gmail.com>
2024-04-07 23:58:55 +0100

    everything

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,22 @@
 const express = require('express');
-//const { MongoClient } = require('mongodb');
+var cors = require("cors");
+const multer = require ("multer")
+const MongoClient = require ("mongodb").MongoClient;
+
 const app = express();
+app.use(cors());
 const port = 3001;
-// const cors = require("cors");
-// const multer = require ("multer")
-const Mongoclient = require ("mongodb").MongoClient;
-
-const CONNECTION_STRING = "mongodb://localhost:27017/"
-//const client = new MongoClient(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
-
-
+const CONNECTION_STRING = "mongodb://localhost:27017/StudentManagementSystem"
 const DATABASENAME="StudentManagementSystem";
+
 var database; 
 
 app.listen(port, () => {
-    Mongoclient.connect(CONNECTION_STRING, (error,client) => {
+    MongoClient.connect(CONNECTION_STRING, (error,client) => {
         database = client.db(DATABASENAME);
         console.log("MongoDB Connection Succesful");
     });
-    console.log(`Backend server is running on http://localhost:${port}`);
+    //.log(`Backend server is running on http://localhost:${port}`);
 });
 
 // async function connectDB() {
