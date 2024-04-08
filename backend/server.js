dc7ceb4ef2676346f9dfa70484cfcf21f50d5fb7
Emmanuel <gazellehunter24@gmail.com>
2024-04-08 13:00:46 +0100

    changed mongo@4.1.0 --save installed cors, installed multer --save installed express --save

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+//gazellehunter24
+//f7y2YWbvDzCqiB4V
+
 const express = require('express');
 var cors = require("cors");
 const multer = require ("multer")
@@ -6,35 +9,42 @@ const MongoClient = require ("mongodb").MongoClient;
 const app = express();
 app.use(cors());
 const port = 3001;
-const CONNECTION_STRING = "mongodb://localhost:27017/StudentManagementSystem"
-const DATABASENAME="StudentManagementSystem";
+const uri = "mongodb+srv://gazellehunter24:f7y2YWbvDzCqiB4V@cluster0.isp2kui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+var databaseName = "studentmanagementsystem";
 
 var database; 
 
-app.listen(port, () => {
-    MongoClient.connect(CONNECTION_STRING, (error,client) => {
-        database = client.db(DATABASENAME);
+app.listen(5038, () => {
+    MongoClient.connect(uri, (error,client) => {
+        database = client.db(databaseName);
         console.log("MongoDB Connection Succesful");
+        console.log(`Backend server is running on http://localhost:${port}`);
+
     });
-    //.log(`Backend server is running on http://localhost:${port}`);
 });
 
-// async function connectDB() {
-//     try {
-//         await client.connect();
-//         console.log("Connected to MongoDB");
-//         // Further operations (CRUD) go here
-//     } catch (error) {
-//         console.error("Could not connect to MongoDB", error);
-//     }
-// }
 
-// connectDB();
 
-// app.listen(port, () => {
-//     console.log(`Backend server is running on http://localhost:${port}`);
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://gazellehunter24:f7y2YWbvDzCqiB4V@cluster0.isp2kui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
 // });
-
-
-
-
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
