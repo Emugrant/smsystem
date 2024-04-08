a7033f9160be22ef33c52037629546a5bc72b842
Emmanuel <gazellehunter24@gmail.com>
2024-04-08 14:26:48 +0100

    backend created

--- backend/api.js
+++
@@ -1,7 +0,0 @@
-
-
-app.get('/api/data', async (req, res) => {
-    const collection = client.db("StudentManagementSystem").collection("Students");
-    const data = await collection.find({}).toArray();
-    res.json(data);
-});
--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ var databaseName = "studentmanagementsystem";
 
 var database; 
 
-app.listen(5038, () => {
+app.listen(port, () => {
     MongoClient.connect(uri, (error,client) => {
         database = client.db(databaseName);
         console.log("MongoDB Connection Succesful");
@@ -23,28 +23,39 @@ app.listen(5038, () => {
     });
 });
 
+app.get('/api/students/list-of-students', (request,response)=>{
+    database.collection("students").find({}).toArray((error,result)=>{
+        response.send(result);
+    })
+})
 
+app.post('/api/students/add-students',multer().none(),(request,response)=>{
+    database.collection("students").count({},function(error,numOfDocs){
+        database.collection("students").insertOne({
+            id:(numOfDocs+1).toString(),
+            name:request.body.name,
+            email:request.body.email,
+            course:request.body.course,
+        });
+        response.send("Student Added");
+    });
+});
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://gazellehunter24:f7y2YWbvDzCqiB4V@cluster0.isp2kui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+app.put('/api/students/update-students',multer().none(),(request,response)=>{
+    database.collection("students").updateOne({
+        id:request.body.id
+    },{ $set:{
+        name:request.body.name,
+        email:request.body.email,
+        course:request.body.course,
+    }});
+    response.send("Student Updated");
+});
+    
+
+app.delete('/api/students/delete-students',multer().none(),(request,response)=>{
+    database.collection("students").deleteOne({
+        id:request.body.id
+    });
+    response.send("Student Deleted");
+});
