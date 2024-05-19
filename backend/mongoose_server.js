45478f9247353bdb0a704776591267cfcb8540f0
Emmanuel - Thinkpad <116040089+Emugrant@users.noreply.github.com>
2024-05-19 23:53:03 +0100

    set up the connection using the documentation
    - [ ] set up crud operations

--- backend/mongoose_server.js
+++ backend/mongoose_server.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const cors = require("cors"); // - [ ] what is cors
-const multer = require ("multer")// - [ ] what is multer
-const MongoClient = require ("mongodb").MongoClient;
+const MongoClient = require ("mongodb").MongoClient; //mongoose alt
 const { ObjectId } = require('mongodb');  // Import ObjectId from mongodb to handle conversion of string _id to MongoDB's ObjectId
 const app = express();
 const mongoose = require('mongoose');
@@ -24,12 +22,10 @@ async function main() {
         app.listen(port, () => {
             console.log(`Backend server is running on http://localhost:${port}`);
         });
-    }
-    catch (error) {
+    } catch (error) {
         console.error("MongoDB Connection Failed: ", error);
     }
-}
-
+};
 main();
 
 //Routes HTTP GET requests to the specified path with the specified callback functions.
@@ -109,11 +105,3 @@ app.delete('/api/students/delete-students/:id', (request, response) => {
             response.status(500).send("Error deleting student: " + error);
         });
 });
- 
- 
-
-// - [x] what does multer do and what would happen to the code if it was removed?
-//          Multer is a middleware for handling multipart/form-data, which is primarily used for uploading files. 
-//          In this code, multer().none() is used to handle data that isn't associated with files, ensuring that only text fields are parsed and available in request.body.
-//          If removed: The code would fail to parse the incoming request data correctly in POST and PUT methods as Express does not handle multipart bodies by default. 
-//          This would lead to undefined fields when trying to access request.body parameters.
