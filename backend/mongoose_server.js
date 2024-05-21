b5e68f2ee2d914bcd638ac53669f6f72dbc9602c
Emmanuel <gazellehunter24@gmail.com>
2024-05-21 23:57:24 +0100

    Following MERN Stack Tutorial - book Stor Project on freeCodeCamp youtube

--- backend/mongoose_server.js
+++ backend/mongoose_server.js
@@ -9,25 +9,36 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 // app.use(cors()); //Applies CORS middleware to allow cross-origin requests. (from differing ip addresses)
 
 const port = 3001;
-const uri = "mongodbawait+srv://gazellehunter24:f7y2YWbvDzCqiB4V@cluster0.isp2kui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodbawait+srv://gazellehunter24:f7y2YWbvDzCqiB4V@cluster0.isp2kui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; //username: gazellehunter24//password: f7y2YWbvDzCqiB4V//cluster0: isp2kui.mongodb.net
 const databaseName = "studentmanagementsystem";
-//username: gazellehunter24
-//password: f7y2YWbvDzCqiB4V
-//cluster0: isp2kui.mongodb.net
+
 
 //https://www.mongodb.com/docs/drivers/node/current/quick-start/download-and-install/
 //https://www.youtube.com/watch?v=-42K44A1oMA&t=784s
 
 
-app.listen(port, () => {
-    try{
-        mongoose.connect(uri + "/" + databaseName);
-        console.log("MongoDB Connection Successful");
-        console.log(`Backend server is running on http://localhost:${port}`);
-    }catch(error){
-        console.error("MongoDB Connection Failed: ", error);
-    }
+
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to MERN Stack Tutorial');
 });
+                                            
+mongoose // this block isn't running. Is there womething wrong with imports or syntax?
+    .connect(uri)
+    .then(() => { // What does '.then()' do???
+        console.log('App connected to database;');
+        app.listen(port, () => {
+            console.log(`App is listening on port: ${port}`)
+            try{
+                mongoose.connect(uri + "/" + databaseName);
+                console.log("MongoDB Connection Successful");
+                console.log(`Backend server is running on http://localhost:${port}`);
+            }catch(error){
+                console.error("MongoDB Connection Failed: ", error);
+            }
+        });
+// 9:10
 
 
 const studentSchema = new mongoose.Schema({
@@ -53,83 +64,3 @@ studentSchema.methods.getStudentName(student1);
 
 student1.save(); // save the student1 document to the database
 
-
-
-
-
-
-
-// ----------------- CRUD OPERATIONS ----------------- //
-
-// app.get('/api/students/all-students', (request,response)=>{
-//     database.collection("students").find({}).toArray((error,result)=>{
-//         response.send(result);        
-//     })
-// })
-
-// app.get('/api/students/:_id', (req, res) => {
-//     const studentId = req.params._id; // Get the _id from the URL parameter
-
-//     database.collection("students").findOne({_id: ObjectId(studentId)}, (error, result) => {
-//         if (error) {
-//             res.status(500).send("Error accessing the database");
-//         } else if (!result) {
-//             res.status(404).send("No student found with that ID");
-//         } else {
-//             res.send(result);
-//         }
-//     });
-// });
-
-// app.post('/api/students/add-students',multer().none(),(request,response)=>{ // - [ ] what does multer do and what would happen to the code if it was removed?
-//     database.collection("students").count({},function(error,numOfDocs){
-//         database.collection("students").insertOne({
-//             id:(numOfDocs+1).toString(),
-//             name:request.body.name,
-//             email:request.body.email,
-//             course:request.body.course,
-//         });
-//         response.send("Student Added");
-//     });
-// });
-
-// //The count function in MongoDB is used to count the number of documents that match a certain condition. Here, it's used to count all documents in the students collection ({} matches all documents) to generate a new id for a newly added student.
-
-// app.put('/api/students/update-student/:_id', (req, res) => {
-//     const studentId = req.params._id;  // Extracting the ID from the request parameters
-//     const updateData = {
-//         name: req.body.name,
-//         email: req.body.email,
-//         course: req.body.course
-//     };
-
-//     database.collection("students").updateOne(
-//         { _id: ObjectId(studentId) }, // Filter document by _id to find the specific student
-//         { $set: updateData }  // Use the $set operator to update the student document
-//     ).then(result => {
-//         if (result.matchedCount === 0) {
-//             res.status(404).send("No student found with that ID");  // No document matches the provided _id
-//         } else {
-//             res.send("Student updated successfully");  // Successfully updated the document
-//         }
-//     }).catch(error => {
-//         res.status(500).send("Error updating student: " + error);  // Handling potential errors
-//     });
-// });
-
-// // The $set operator in a MongoDB update operation specifies the fields to be updated in the document. It replaces the value of a field with the specified value without modifying any other fields in the document. In this code, it updates name, email, and course fields of a student document.
-    
-
-// app.delete('/api/students/delete-students/:id', (request, response) => {
-//     const studentId = request.params.id;
-//     database.collection("students").deleteOne({ _id: new require('mongodb').ObjectId(studentId) })
-//         .then(result => {
-//             if (result.deletedCount === 0) {
-//                 return response.status(404).send("No student found with that ID");
-//             }
-//             response.send("Student Deleted");
-//         })
-//         .catch(error => {
-//             response.status(500).send("Error deleting student: " + error);
-//         });
-// });
