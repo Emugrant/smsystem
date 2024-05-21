547a7819308dc9afa1fdb8775853248fb78dd233
Emmanuel <gazellehunter24@gmail.com>
2024-05-22 00:20:08 +0100

    added models folder

---
+++ backend/models/studentModel.js
@@ -0,0 +1,27 @@
+//10:40
+
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    course: String
+});
+
+const Student = mongoose.model('Student', studentSchema); 
+// a model is a class with which we construct documents. In this case, each document will be a student with properties and behaviors as declared in our schema.
+//a schema is a blueprint for a document in a MongoDB collection. It defines the shape of the documents within that collection.
+// an instance of a model is called a document. 
+
+const student1 = new Student({ name: 'John Doe', email: 'example@gmail.com', course: 'Computer Science' });
+
+
+
+studentSchema.methods.getStudentName = function () {
+    return this.name;
+};
+
+studentSchema.methods.getStudentName(student1);
+
+student1.save(); // save the student1 document to the database
+
