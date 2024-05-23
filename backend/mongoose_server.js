e5a1bb5e2e15c98f42fc00078a8db1ae2848f9b2
emmanuelapata <eapata24@gmail.com>
2024-05-23 11:29:46 +0100

    updated uri and installed mongoose

--- backend/mongoose_server.js
+++ backend/mongoose_server.js
@@ -2,19 +2,17 @@
 //https://www.youtube.com/watch?v=-42K44A1oMA&t=784s
 
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-
-app.use(express.json()); // This tells the Express application to use middleware that automatically parses JSON formatted request bodies.
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-// app.use(cors()); //Applies CORS middleware to allow cross-origin requests. (from differing ip addresses)
 
 const port = 3001;
 const uri = "mongodbawait+srv://gazellehunter24:f7y2YWbvDzCqiB4V@cluster0.isp2kui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; //username: gazellehunter24//password: f7y2YWbvDzCqiB4V//cluster0: isp2kui.mongodb.net
+const uriWithCollectionName = "mongodb+srv://gazellehunter24:f7y2YWbvDzCqiB4V@cluster0.isp2kui.mongodb.net/studentmanagementsystem?retryWrites=true&w=majority&appName=Cluster0"; //username: gazellehunter24//password: f7y2YWbvDzCqiB4V//cluster0: isp2kui.mongodb.net
 const databaseName = "studentmanagementsystem";
 
-
-
+const app = express();
+app.use(express.json()); // This tells the Express application to use middleware that automatically parses JSON formatted request bodies.
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// app.use(cors()); //Applies CORS middleware to allow cross-origin requests. (from differing ip addresses)
 
 app.get('/', (request, response) => {
     console.log(request);
@@ -22,7 +20,7 @@ app.get('/', (request, response) => {
 });
                                             
 mongoose // this block isn't running. Is there womething wrong with imports or syntax?
-    .connect(uri)
+    .connect(uriWithCollectionName)
     .then(() => { // What does '.then()' do???
         console.log('App connected to database;');
         app.listen(port, () => {
